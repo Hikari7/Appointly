70fb317ad6b0945505e81b5610851f23e6791965
Yuri <snnp.ad.pnts@gmail.com>
2023-03-16 16:14:28 -0700

    Add time selector

--- client/src/components/Public/BaseCalendar.jsx
+++ client/src/components/Public/BaseCalendar.jsx
@@ -1,5 +1,7 @@
-import moment from 'moment';
 import { useEffect, useState } from 'react';
+import { HashLink } from "react-router-hash-link"
+
+import moment from 'moment';
 
 import { createCalendar, getNextMonth, getPrevMonth } from "../../utils/calenderHelpers";
 import TimeSelector from './TimeSelector';
@@ -13,26 +15,25 @@ const BaseCalendar = () => {
     const [isOpen, setIsOpen] = useState(false)
 
     const availability = [
-      {mon: {number: "1", time: ["09:00", "09:30", "10:00", "10:30", "11:00"]}},
-      {tue: {number: "2", time: ["09:00", "09:30", "10:00", "10:30", "11:00"]}},
-      {wed: {number: "3", time: ["09:00", "09:30", "10:00", "10:30", "11:00"]}},
-      {thu: {number: "4", time: ["09:00", "09:30", "10:00", "10:30", "11:00"]}},
-      {fri: {number: "5", time: ["09:00", "09:30", "10:00", "10:30", "11:00"]}},
+      {1: ["09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30"]},
+      {2: ["09:30", "10:00", "10:30", "11:00"]},
+      {3: ["10:00", "09:30", "10:30", "11:00"]},
+      {4: ["10:30", "09:30", "10:00", "11:00"]},
+      {5: ["11:00", "09:30", "10:00", "10:30"]},
     ]
 
     const availableDow = []
     availability.map(eachDow => {
-      if(Object.values(eachDow)[0].time.length > 0){
-        availableDow.push(Object.values(eachDow)[0].number)
+      if(Object.values(eachDow)[0].length > 0){
+        availableDow.push(Object.keys(eachDow)[0])
       }
     })
-
     // console.log(availableDow);
-    
+
     useEffect(() => {
       setCalendarDate(createCalendar(currentDate))
     }, [currentDate])
-  
+
     const today = moment()
     const year = currentDate.get('year')
     const month = currentDate.format('MMM')
@@ -48,14 +49,15 @@ const BaseCalendar = () => {
       }
     }
 
-    const handleClickDate = (date) => {
+    const handleClickDate = (date, timeArray) => {
       setIsOpen(true)
       setSlectedDate(date)
-      setDisplayTime
+      setDisplayTime(Object.values(timeArray[0])[0])
     }
 
     return (
-        <div className='md:w-1/3'>
+      <div className='flex flex-col md:flex-row'>
+        <div className='md:w-1/2 p-10'>
           <div className="flex justify-between items-center mb-5">
             <svg onClick={() => handleChangeMonth("prev")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
@@ -74,48 +76,42 @@ const BaseCalendar = () => {
                 {week.map((day, index) => {
                   if(currentMonth !== day.month){              
                     return (
-                        <div key={index} className='flex-1 flex justify-center items-center'>
-                            <div onClick={() => handleClickDate(`${day.month}-${day.date}`)} className="text-center text-gray-300">{day.date}</div>
-                        </div>
+                      <div key={index} className='flex-1 flex justify-center items-center'>
+                        <div className="text-center text-gray-300">{day.date}</div>
+                      </div>
                     )
                   }else if(moment(`${day.month}-${day.date}`)._i === today.format("YYYY-MM-DD")){
-                    if(availableDow.includes(moment(`${day.month}-${day.date}`).format('d'))){
-                      return (
-                        <div key={index} className='flex-1 flex justify-center items-center relative group'>
-                            <div className='flex justify-center items-center w-7 h-7 md:w-10 md:h-10 text-white bg-green-400 rounded-full group-hover:bg-green-600'>
-                                <div onClick={() => handleClickDate(`${day.month}-${day.date}`)} className="text-center z-50">{day.date}</div>
-                            </div>
-                        </div>
-                      ) 
-                    }else{
-                      return (
-                          <div key={index} className='flex-1 flex justify-center items-center'>
-                              <div onClick={() => handleClickDate(`${day.month}-${day.date}`)} className="text-center font-bold text-green-400 rounded-full">{day.date}</div>
-                          </div>
-                      )  
-                    }
-                  }else if(availableDow.includes(moment(`${day.month}-${day.date}`).format('d'))){
                     return (
-                        <div key={index} className='flex-1 flex justify-center items-center relative group'>
-                            <div className='flex justify-center items-center w-7 h-7 md:w-10 md:h-10 bg-green-200 rounded-full group-hover:bg-green-400'>
-                                <div onClick={() => handleClickDate(`${day.month}-${day.date}`)} className="text-center z-50">{day.date}</div>
-                            </div>
+                      <div key={index} className='flex-1 flex justify-center items-center'>
+                        <div className="text-center font-bold text-green-400 rounded-full">{day.date}</div>
+                      </div>
+                    ) 
+                  }else if(availableDow.includes(moment(`${day.month}-${day.date}`).format('d'))){
+                    // Get target available time
+                    const timeArray = availability.filter(e => Object.keys(e)[0] === String(moment(`${day.month}-${day.date}`).format('d')))
+                    return (
+                      <div key={index} className='flex-1 flex justify-center items-center relative group'>
+                        <div className='flex justify-center items-center w-[2rem] h-[2rem] md:w-10 md:h-10 bg-green-200 rounded-full group-hover:bg-green-400'>
+                          <HashLink smooth to="#timeSelect" onClick={() => handleClickDate(`${day.month}-${day.date}`, timeArray)} className="text-center z-50">{day.date}</HashLink>
                         </div>
+                      </div>
                     )  
                   }else{
                     return (
-                        <div key={index} className='flex-1 flex justify-center items-center'>
-                            <div onClick={() => handleClickDate(`${day.month}-${day.date}`)} className="text-center">{day.date}</div>
-                        </div>
+                      <div key={index} className='flex-1 flex justify-center items-center'>
+                        <div className="text-center">{day.date}</div>
+                      </div>
                     )  
                   }
                 })}
               </div>
             ))
           }
-          {isOpen && <TimeSelector timeArr={displayTime}/>}
+          <hr className='mt-5'/>
         </div>
-      )
+        {isOpen && <TimeSelector timeArray={displayTime} selectDate={selectedDate} /> }
+      </div>
+    )
 }
 
 export default BaseCalendar
--- client/src/components/Public/TimeSelector.jsx
+++ client/src/components/Public/TimeSelector.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import moment from "moment" 
+import { useState, useEffect } from "react"
 
-const TimeSelector = ({ timeArr }) => {
+const TimeSelector = ({ timeArray, selectDate }) => {
+  const [selectedTime, setSlectedTime] = useState("")
+  const [selectedDate, setSelectedDate] = useState(moment(selectDate).format('YYYY-MM-DD'))
 
+  useEffect(() => {
+    setSelectedDate(moment(selectDate).format('YYYY-MM-DD'))
+    setSlectedTime("")
+  }, [selectDate])
 
   return (
-    <>
-        
-    </>
+    <div id='timeSelect' className='flex flex-col justify-center w-1/2 md:w-1/3 mx-auto my-5'>
+      <div className="flex flex-col justify-content items-center">
+        <div className="flex items-center mb-2">
+          <div className="w-[30%] mr-2">Date:</div>
+          <div className="text-center w-[8rem] h-[1.9rem] py-.5 text-lg border border-gray-500 rounded">{selectedDate}</div>
+        </div>
+        <div className="flex items-center">
+          <div className="w-[30%] mr-2">Time:</div>
+          <div className="text-center w-[8rem] h-[1.9rem] py-.5 text-lg border border-gray-500 rounded">{selectedTime}</div>
+        </div>
+      </div>
+      <div id='timeSelect' className='flex flex-col justify-center my-8 gap-5'>
+        {timeArray && 
+          timeArray.map((eachTime, index) => (
+            <button onClick={() => setSlectedTime(eachTime)} key={index} className='py-1 px-3 border-2 border-green-400 rounded hover:bg-green-400 hover:text-white'>
+              {eachTime}
+            </button>
+          ))        
+        }
+      </div>
+    </div>
   )
 }
 
