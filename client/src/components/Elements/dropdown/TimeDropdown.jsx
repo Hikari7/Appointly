b4a2e4d6cb572d9f2c03cedd42c491b8c3869ecd
Yuri <snnp.ad.pnts@gmail.com>
2023-03-23 23:14:57 -0700

    Add display time dropdown loginc on availability component

--- client/src/components/Elements/dropdown/TimeDropdown.jsx
+++ client/src/components/Elements/dropdown/TimeDropdown.jsx
@@ -1,22 +1,23 @@
 import { useState } from "react"
 import moment from 'moment';
 
-const TimeDropdown = () => {
+const TimeDropdown = ({ left }) => {
     //Create selectable time array
     const timeArr = []
     let baseTime = moment('2023-03-20 00:00')
-    while(baseTime.format('HH:mm') !== "23:30"){
+    while(baseTime.format('HH:mm') !== "23:00"){
         timeArr.push(baseTime.format('HH:mm'))
         baseTime.add(30, 'm')
         if(baseTime.format('HH:mm') === "23:00"){
+            timeArr.push("23:00")
             timeArr.push("23:30")
         }
     }
 
   return (
-    <div className='flex flex-col bg-white m-4 px-2 border-2 border-green-400 rounded-lg w-fit h-[400%] overflow-y-scroll absolute top-[55%] -left-[7%]'>
+    <div className={`flex flex-col bg-white m-4 px-1.5 border-2 border-green-400 rounded-lg w-fit h-[400%] overflow-y-scroll absolute top-[55%] left-[${left}%] z-50`}>
     {timeArr && timeArr.map((eachTime, index) => (
-        <div key={index} className='p-1'>
+        <div key={index} className='p-1 hover:bg-gray-200 rounded-lg'>
             {eachTime}
         </div>
     ))}
--- client/src/components/User/WeeklyAvailability.jsx
+++ client/src/components/User/WeeklyAvailability.jsx
@@ -8,25 +8,36 @@ import TimeDropdown from '../Elements/dropdown/TimeDropdown'
 
 const initialState = [
   {Sun: false, time: [{start: "09:00", end: "17:00"}]},
-  {Mon: false, time: []},
-  {Tue: false, time: []},
-  {Wed: false, time: []},
-  {Thu: false, time: []},
-  {Fri: false, time: []},
-  {Sat: false, time: []},
+  {Mon: false, time: [{start: "09:00", end: "17:00"}]},
+  {Tue: false, time: [{start: "09:00", end: "17:00"}]},
+  {Wed: false, time: [{start: "09:00", end: "17:00"}]},
+  {Thu: false, time: [{start: "09:00", end: "17:00"}]},
+  {Fri: false, time: [{start: "09:00", end: "17:00"}]},
+  {Sat: false, time: [{start: "09:00", end: "17:00"}]},
 ]
 
 const WeeklyAvailability = () => {
   const availability = useSelector((state) => state.availability.availability)
   const dispatch = useDispatch()
   const [selectedDow, setSelectedDow] = useState(initialState)
-  // const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-  const [dropdownOpen, setDropdownOpen] = useState(null)
-  const inputRefs = useRef([])
+  const [selectedItem, setSelectedItem] = useState("")
+  const [targetElem, setTargetElem] = useState(null)
 
   useEffect(() => {
-    // console.log(selectedDow);
-  }, [selectedDow])
+    const elem = targetElem
+    if(!elem) return 
+
+    const handleCloseTimeDropdown = (e) => {
+      if(!(elem === e.target)){
+        setSelectedItem("")
+      }
+    }
+
+    document.addEventListener("click", handleCloseTimeDropdown);
+    return () => {
+      document.removeEventListener("click", handleCloseTimeDropdown);
+    };
+  }, [targetElem])
 
   const handleCheckbox = (e) => {
     const targetDow = e.target.value
@@ -49,17 +60,13 @@ const WeeklyAvailability = () => {
 
   const displayDow = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
-  const displayTimeDropdown = (id) => {
-    // setIsDropdownOpen(!isDropdownOpen)
-    console.log();
-    if(!dropdownOpen){
-      setDropdownOpen(id)
-    }else{
-      setDropdownOpen(null)
-    }
+  const displayTimeDropdown = (id, elem) => {
+    setSelectedItem(id)
+    setTargetElem(elem)
+
   }
 
-  const handleMethod = (meyhod) => {
+  const handleMethod = (meyhod, targetObj) => {
     if(meyhod === "delete"){
       
     }else if('add'){
@@ -70,9 +77,9 @@ const WeeklyAvailability = () => {
   }
 
   return (
-    <form className='flex flex-col shrink p-5 md:w-[60%]'>
-      {displayDow.map((eachDow, index) => (
-        <div key={index} className='flex justify-between width-full my-4'>
+    <form className='flex flex-col p-5 md:w-[60%]'>
+      {displayDow.map((eachDow, dowIndex) => (
+        <div key={dowIndex} className='flex justify-between width-full my-4'>
           <div className='flex justify-between w-full'>
             <label className='flex items-center gap-3 font-bold w-[20%]'>
               <input 
@@ -86,35 +93,32 @@ const WeeklyAvailability = () => {
             {selectedDow.find(elem => Object.keys(elem)[0] === eachDow)[eachDow]
               ? (
                 selectedDow.find(elem => Object.keys(elem)[0] === eachDow).time.map((startEndObj, index) => {
-                  const startId = uuidv4()
-                  const endId = uuidv4()
                   return (
                   <div key={index} className='flex items-center gap-3 w-[80%] ml-4 relative'>
                     <div className='relativ'>
                       <input
-
                         type="text"
+                        id={`${eachDow}Start`}
                         value={startEndObj.start}
-                        id={startId}
-                        onClick={() => displayTimeDropdown(startId)}
+                        onClick={(e) => displayTimeDropdown(e.target.id, e.currentTarget)}
                         onChange={(e) => e.target.value}
                         className='border border-gray-700 rounded-lg w-16 h-7 text-center focus:border-green-400'
                       />
-                      {dropdownOpen === startId  && <TimeDropdown />}
+                      {selectedItem === `${eachDow}Start` && <TimeDropdown left={"-7"} />}
                     </div>
                     -
                     <div className='relativ'>
                       <input 
                         type="text"
                         value={startEndObj.end}
-                        id={endId}
-                        onClick={() => displayTimeDropdown(endId)}
+                        id={`${eachDow}End`}
+                        onClick={(e) => displayTimeDropdown(e.target.id)}
                         onChange={(e) => e.target.value}
                         className='border border-gray-700 rounded-lg w-16 h-7 text-center'
                       />
-                      {dropdownOpen === endId && <TimeDropdown />}
+                      {selectedItem === `${eachDow}End` && <TimeDropdown left={"31"} />}
                     </div>
-                    <svg onClick={() => handleClick("add")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <svg onClick={() => handleClick("delete", startEndObj)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                     </svg>
                   </div>
