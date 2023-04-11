778c285a5b2cff862c0640cf6c04df4184273d31
Hikari Kobe <h.kobe712@gmail.com>
2023-04-10 17:08:55 -0700

    added setting useraccount information

--- client/src/api/userSettingApi.js
+++ client/src/api/userSettingApi.js
@@ -1,9 +1,10 @@
 import axiosClient from "./axiosClient";
 
 const userSettingApi = {
-  update: (uid, params) => axiosClient.put(`/user/${uid}/edituserinfo`, params),
+  // update: (uid, params) => axiosClient.put(`/user/${uid}/edituserinfo`, params),
   // reset: (uid, params) =>
   //   axiosClient.post(`/user/${uid}/resetpassword`, params),
+  updateUserInfo: (uid, params) => axiosClient.put(`user/${uid}/updateuserinfo`, params),
 };
 
 export default userSettingApi;
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,64 +1,71 @@
 const {
-    fetchAppointment,
-    setAvailability,
-    rescheduleMtg,
-    deleteAppointment,
-    fetchUserAvailability,
-    updateUsername,
-} = require('../middleware/user.service')
+  fetchAppointment,
+  setAvailability,
+  rescheduleMtg,
+  deleteAppointment,
+  fetchUserAvailability,
+  updateUsername,
+} = require("../middleware/user.service");
 
 exports.fetchAppointmentController = async (req, res) => {
-    const { uid } = req.params
-    const allAppointments = await fetchAppointment(uid)
-    return res.json(allAppointments);
-}
+  const { uid } = req.params;
+  const allAppointments = await fetchAppointment(uid);
+  return res.json(allAppointments);
+};
 
 exports.fetchAvailabilityController = async (req, res) => {
-    const { uid } = req.params
-    const userAvailability = await fetchUserAvailability(uid)
-    return res.json(userAvailability)
-}
+  const { uid } = req.params;
+  const userAvailability = await fetchUserAvailability(uid);
+  return res.json(userAvailability);
+};
 
 exports.setAvailabilityController = async (req, res) => {
-    try {
-        const { uid } = req.params
-        const newAvailability = await setAvailability(uid, req.body)
-        return res.json(newAvailability)
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send({errorMessage: "Something went wrong. Please try again."});
-    }
-}
+  try {
+    const { uid } = req.params;
+    const newAvailability = await setAvailability(uid, req.body);
+    return res.json(newAvailability);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ errorMessage: "Something went wrong. Please try again." });
+  }
+};
 
 exports.rescheduleMtgController = async (req, res) => {
-    try {
-        const { appointmentid } = req.params
-        const { changedDateTime } = req.body
-        const changedMtg = await rescheduleMtg(appointmentid, changedDateTime)
-        return res.json(changedMtg)
-    } catch (error) {
-        return res.status(400).send({errorMessage: "Something went wrong. Please try again."});
-    }
-}
+  try {
+    const { appointmentid } = req.params;
+    const { changedDateTime } = req.body;
+    const changedMtg = await rescheduleMtg(appointmentid, changedDateTime);
+    return res.json(changedMtg);
+  } catch (error) {
+    return res
+      .status(400)
+      .send({ errorMessage: "Something went wrong. Please try again." });
+  }
+};
 
 exports.deleteMtgController = async (req, res) => {
-    try {
-        const { appointmentid } = req.params
-        await deleteAppointment(appointmentid)
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send({errorMessage: "Something went wrong. Please try again."});
-    }
-}
+  try {
+    const { appointmentid } = req.params;
+    await deleteAppointment(appointmentid);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ errorMessage: "Something went wrong. Please try again." });
+  }
+};
 
 exports.updateUserInfoController = async (req, res) => {
-    try {
-        const { uid } = req.params
-        const newUserInfo = await updateUsername(uid, req.data)
-        return res.json(newUserInfo)
-    } catch (error) {
-        console.log(error);
-        return res.status(400).send({errorMessage: "Something went wrong. Please try again."});
-
-    }
-}
+  try {
+    const { uid } = req.params;
+    const newUserInfo = await updateUsername(uid, req.body);
+    return res.json(newUserInfo);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .send({ errorMessage: "Something went wrong. Please try again." });
+  }
+};
