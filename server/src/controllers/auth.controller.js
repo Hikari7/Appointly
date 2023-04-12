368ef8e5b23760dd411459d76d967ed527dd0d07
Yuri <snnp.ad.pnts@gmail.com>
2023-04-12 14:52:46 -0700

    Remove unnecessary code

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const { signUp, login, userCheck } = require("../middleware/auth.service");
+const { signUp, login } = require("../middleware/auth.service");
 
 exports.signUpController = async (req, res) => {
   const { username, email, password } = req.body;
@@ -13,40 +13,10 @@ exports.loginController = async (req, res) => {
     const loginService = await login(email, password);
     console.log({loginService});
     loginService.errorMessage && res.status(400).send({ errorMessage });
-    return res
-      .cookie("token", loginService.token, { httpOnly: true })
-      .json(loginService);
+    return res      .json(loginService);
   } catch (error) {
     return res
       .status(400)
       .send({ errorMessage: "Something went wrong. Please try again." });
   }
 };
-
-exports.checkToken = async (req, res, next) => {
-  try {
-    const checkToken = req.headers.cookie;
-    const token = checkToken.split("=")[1];
-
-    if (token) {
-      JWT.verify(token, jwtSecret, (error, decoded) => {
-        if (error) {
-          return res.status(400).send({ errorMessage: "Failed to authrize" });
-        } else {
-          req.decoded = decoded;
-          next();
-        }
-      });
-    } else {
-      return res.status(400).send({ errorMessage: "There is no token" });
-    }
-  } catch (error) {
-    return res
-      .status(400)
-      .send({ errorMessage: "Something went wrong. Please try again." });
-  }
-};
-
-exports.resetPassword = async () => {};
-
-exports.passwordResetController = async () => {};
