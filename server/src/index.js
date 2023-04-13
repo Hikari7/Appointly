92ce6dc48f3f33445666c91a92e8bb821c2f80f5
Hikari Kobe <h.kobe712@gmail.com>
2023-04-12 20:05:37 -0700

    pushed for merge

--- server/src/index.js
+++ server/src/index.js
@@ -6,16 +6,10 @@ require("./utils/mongodb");
 const authRoute = require("./routes/auth.route");
 const appointmentRoute = require("./routes/appointment.route");
 const userRoute = require("./routes/user.route");
-// const { checkToken } = require('./controllers/auth.controller')
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:5173",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -23,7 +17,6 @@ app.use("/api", (_, res) => res.json({ message: "Health check" }));
 app.use("/auth", authRoute);
 app.use("/appointment", appointmentRoute);
 app.use("/user", userRoute);
-// app.use('/user', checkToken, userRoute)
 
 app.use((req, res, next) => {
   const err = new Error("Route not found.");
--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
 
-const { loginController, signUpController, passwordResetController } = require('../controllers/auth.controller')
+const { loginController, signUpController } = require('../controllers/auth.controller')
 
 router.post("/login", loginController);
 router.post("/signup", signUpController);
-router.post("/passwordreset", passwordResetController)
 
 module.exports = router
