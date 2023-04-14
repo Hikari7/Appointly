968478bbe97bb9b5d895a6020b3fe00c8397cf1e
Hikari Kobe <h.kobe712@gmail.com>
2023-04-14 15:27:56 -0700

    fixed titleWrapper layout

--- client/src/components/Elements/Wrapper/TitleWrapper.jsx
+++ client/src/components/Elements/Wrapper/TitleWrapper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TitleWrapper = ({ children }) => {
   return (
-    <div className="md:border-r border-neutral border-thin justify-center flex-col flex items-center text-center w-3/12 ">
+    <div className="md:border-r border-neutral border-thin justify-center flex-col flex items-center text-center md:w-3/12 ">
       {children}
     </div>
   );
