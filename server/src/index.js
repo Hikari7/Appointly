7104bbcc5bc8f246057f1514209a5aac919a3192
Yuri <snnp.ad.pnts@gmail.com>
2023-04-20 16:31:37 -0700

    Add cors origin

--- server/src/index.js
+++ server/src/index.js
@@ -9,7 +9,9 @@ const userRoute = require("./routes/user.route");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://appointly-mtg.vercel.app/"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
